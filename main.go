70ede95bfb5a07e2245c5c283182aa0a592ff640
Anjan <kaludios@gmail.com>
2022-03-23 18:54:03 +0530

    Implements the main functionality of raisepr
    the main function does the following:
    1. it gets the various configuration options from the host env variables (useful when containerizing)
    2. does an oauth 2.0 flow with github
    3. locally clones the configured repo using the token from previous step
    4. creates a local randomly named branch and checks it out in the working tree
    5. creates the changes and makes a commit and pushes the branch
    6. raises a PR to the default base branch from the newly created branch
    7. print the URL to the created PR and exits

--- main.go
+++ main.go
@@ -3,62 +3,173 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
+	ogh "golang.org/x/oauth2/github"
+
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/google/go-github/v43/github"
 )
 
 func main() {
-	repo := os.Getenv("REPO_NAME")
-	if repo == "" {
-		os.Exit(1)
+	cfg, err := getOptsFromEnv()
+	handleError(err, 2)
+
+	accessToken, httpClient := authenticateWithGithub(cfg)
+
+	// using the http client from oauth didn't work
+	// falling back to using the Access token directly
+	// this might expire and result into failures
+	fs := memfs.New()
+	r, err := git.Clone(memory.NewStorage(), fs, &git.CloneOptions{
+		URL: fmt.Sprintf("https://%s:%s@github.com/%s/%s", cfg.RepoOwner, accessToken, cfg.RepoOwner, cfg.RepoName),
+	})
+
+	handleError(err, 8)
+
+	// create new branch
+	name := randBranchName()
+	branchName := plumbing.NewBranchReferenceName(name)
+	headRef, err := r.Head()
+	handleError(err, 9)
+
+	ref := plumbing.NewHashReference(branchName, headRef.Hash())
+	err = r.Storer.SetReference(ref)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	owner := os.Getenv("REPO_OWNER")
-	if owner == "" {
-		os.Exit(2)
+	worktree, err := r.Worktree()
+	handleError(err, 10)
+
+	// checkout new branch
+	worktree.Checkout(&git.CheckoutOptions{
+		Branch: ref.Name(),
+	})
+
+	// make random change to the Makefile
+	f, err := worktree.Filesystem.OpenFile("Makefile", os.O_RDWR, 0666)
+	handleError(err, 11)
+	defer f.Close()
+
+	buf, err := ioutil.ReadAll(f)
+	handleError(err, 12)
+
+	buf = append(buf, []byte("\n# this is a change made with the oAuth app\n")...)
+	f.Write(buf)
+
+	_, err = worktree.Add("Makefile")
+	handleError(err, 13)
+
+	_, err = worktree.Commit("Just a simple man's simple commit msg", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Anjan Nath",
+			Email: "kaludios@gmail.com",
+			When:  time.Now(),
+		},
+	})
+
+	handleError(err, 14)
+
+	// push the changes to the branch
+	err = r.Push(&git.PushOptions{})
+	handleError(err, 15)
+
+	// create PR
+	gh := github.NewClient(httpClient)
+	newPR := &github.NewPullRequest{
+		Title:               github.String("Test PR from raisepr app"),
+		Head:                github.String(branchName.String()),
+		Base:                github.String("main"),
+		Body:                github.String("Test raisepr oAuth app"),
+		MaintainerCanModify: github.Bool(true),
+	}
+	pullReq, _, err := gh.PullRequests.Create(context.Background(), cfg.RepoOwner, cfg.RepoName, newPR)
+	handleError(err, 16)
+
+	fmt.Println("PR Created at: ", *pullReq.URL)
+}
+
+// startCallbackHttpServer listens on port 9999 of localhost
+// and it expects the `code` and `state` parameters to be
+// passed to it after user provides consent
+func startCallbackHttpServer(codeCh chan string, state string) error {
+	var srv = http.Server{
+		Addr: "localhost:9999",
 	}
 
-	client_id := os.Getenv("CLIENT_ID")
-	if client_id == "" {
-		os.Exit(3)
+	shutdownSrv := make(chan bool)
+
+	go func() {
+		<-shutdownSrv
+		fmt.Println("Shutting down server ...")
+		if err := srv.Shutdown(context.Background()); err != nil {
+			fmt.Println("ERROR Trying to shutdown http server", err)
+		}
+	}()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		if params.Get("state") != state {
+			fmt.Println("***State value doesn't match, Possible CSRF, Aborting***")
+			os.Exit(5)
+		}
+		codeCh <- params.Get("code")
+		close(shutdownSrv)
+	})
+
+	// start http server to accept req from oauth callback
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		return err
 	}
 
-	secret := os.Getenv("SECRET")
-	if secret == "" {
-		os.Exit(4)
-	}
-	// TODO: get base branch, against which the PR is to be created
-	// TODO: get VCS provider name or url
+	return nil
+}
 
-	// authenticate with github
+// authenticateWithGithub returns the Access token and http.Client
+// it does an oAuth 2.0 three pronged flow by making the initial
+// request using the ClientID to retrieve the URL where user can
+// provide his consent, then it  starts http server to intercept
+// the initial access code and finally using the access code
+// retrieves the Access token
+func authenticateWithGithub(cfg config) (string, *http.Client) {
 	ctx := context.Background()
 	conf := &oauth2.Config{
-		ClientID:     client_id,
-		ClientSecret: secret,
+		ClientID:     cfg.ClientID,
+		ClientSecret: cfg.ClientSecret,
 		Scopes:       []string{"repo"},
-		Endpoint:     github.Endpoint,
+		Endpoint:     ogh.Endpoint,
 	}
 
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+	// state value to pass during oauth flow, used for CSRF detection
+	state := "foobar"
+
+	// In a webapp we'll Redirect user to url to ask for permission
+	// since this is headless/cli/service we just print the url and
+	// ask user to visit it using their browser
+	url := conf.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	fmt.Println("Visit the URL using your browser to grant access: ", url)
+
+	codeCh := make(chan string)
+	go startCallbackHttpServer(codeCh, state)
+
+	code := <-codeCh
+	close(codeCh)
 
 	// Use the authorization code that is pushed to the redirect
 	// URL. Exchange will do the handshake to retrieve the
 	// initial access token. The HTTP Client returned by
 	// conf.Client will refresh the token as necessary.
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatal(err)
-	}
 	tok, err := conf.Exchange(ctx, code)
-	if err != nil {
-		log.Fatal(err)
-	}
+	handleError(err, 7)
 
-	fmt.Println(tok.AccessToken)
+	return tok.AccessToken, conf.Client(ctx, tok)
 }
